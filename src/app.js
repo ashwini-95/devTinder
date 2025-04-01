67bf920e78a39daecaa1b37a7ef09cedc7349647
ashwini-95 <ashwinisirpa95@gmail.com>
2025-04-01 15:35:57 +0530

    regex in routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,8 @@
 const express = require("express");
 const app = express();
 
-//these are request handlers
-app.get("/user", (req, res) => {
-  res.send("This is a GET Call");
-});
-app.post("/user", (req, res) => {
-  res.send("POST - user added successfully");
-});
-app.delete("/user/1", (req, res) => {
-  res.send("Delete - Deleted successfully");
-});
-app.use("/", (req, res) => {
-  res.send("Welcom Dev Tinders!!");
+app.get(/ab?c$/, (req, res) => {
+  res.send({ firstname: "Ashwini", lastname: "Sirpa" });
 });
 app.listen(4001, () => {
   console.log("App is listening on 4001..");
