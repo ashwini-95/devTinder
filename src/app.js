0f8f32680bfe32a8b519c4be572305f71bc2c3d3
ashwini-95 <ashwinisirpa95@gmail.com>
2025-04-01 17:18:20 +0530

    handling multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const app = express();
-// /ab?c, /ab+c, /ab*c, patterns - /a(bc)+d //
-app.get("/ab(c?)d", (req, res, next) => {
-  res.send({ firstname: "Ashwini", lastname: "Sirpa" });
-});
-app.get("/abc", (req, res) => {
-  console.log(req.query);
-  res.send({ firstname: "Ashwini", lastname: "Sirpa" });
-});
 
-app.get("/abc/:id", (req, res) => {
-  console.log(req.params);
-  res.send({ firstname: "Ashwini", lastname: "Sirpa" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Route handler 1!");
+    // res.send("Route Handler!");
+    next();
+    res.send("Route Handler!");
+  },
+  (req, res) => {
+    console.log("Route handler 2!!");
+    res.send("Route Handler!!");
+  }
+);
 app.listen(4001, () => {
   console.log("App is listening on 4001..");
 });
