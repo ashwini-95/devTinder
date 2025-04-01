7feb8b5042cb1dd40cc2e9124ba2d53af8020b65
ashwini-95 <ashwinisirpa95@gmail.com>
2025-04-01 15:58:41 +0530

    playing with routes and installed express 4+

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+// /ab?c, /ab+c, /ab*c, patterns - /a(bc)+d //
+app.get("/ab(c?)d", (req, res, next) => {
+  res.send({ firstname: "Ashwini", lastname: "Sirpa" });
+});
+app.get("/abc", (req, res) => {
+  console.log(req.query);
+  res.send({ firstname: "Ashwini", lastname: "Sirpa" });
+});
 
-app.get(/ab?c$/, (req, res) => {
+app.get("/abc/:id", (req, res) => {
+  console.log(req.params);
   res.send({ firstname: "Ashwini", lastname: "Sirpa" });
 });
 app.listen(4001, () => {
