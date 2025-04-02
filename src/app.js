2fec2b83c80c555633fe4864b6f179e8f57afd6d
ashwini-95 <ashwinisirpa95@gmail.com>
2025-04-02 16:59:21 +0530

    get apis of user

--- src/app.js
+++ src/app.js
@@ -6,12 +6,6 @@ const app = express();
 app.use(express.json());
 app.post("/signup", async (req, res) => {
   //creating an instance of user model
-  // const user = new User({
-  //   firstName: "Yashu",
-  //   lastName: "Dubb",
-  //   emailId: "yashu@dubb.com",
-  //   password: "john@123",
-  // });
   try {
     console.log(req.body);
     const user = new User(req.body);
@@ -21,7 +15,29 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error in adding user to the database");
   }
 });
-
+//get feed of all users
+app.get("/feed", async (req, res) => {
+  try {
+    const feed = await User.find();
+    res.send(feed);
+  } catch (error) {
+    res.status(400).send("Error in getting users data");
+  }
+});
+// get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.email;
+    const user = await User.find({ emailId: userEmail });
+    if (user.length > 0) {
+      res.send(user);
+    } else {
+      res.status(404).send("user not found");
+    }
+  } catch (error) {
+    res.status(400).send("some error occured");
+  }
+});
 connectDB()
   .then(() => {
     console.log("Database connected succesfully!!!");
