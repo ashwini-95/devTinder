fb7f905b7db261837ee09952d596855377085431
ashwini-95 <ashwinisirpa95@gmail.com>
2025-04-02 12:26:52 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
-
-app.get("/user/data", userAuth, (req, res, next) => {
-  res.send("sent all user data");
+app.get("/getalldata", (req, res) => {
+  try {
+    throw new Error("asfsdfsdf");
+    res.send("data sent!!");
+  } catch (error) {
+    res.status(500).send("some Error occured");
+  }
 });
-app.post("/user/login", (req, res) => {
-  res.send("loggedin successfully");
-});
-app.get("/admin/getAllData", (req, res) => {
-  res.send("sent all data");
-});
-app.delete("/admin/deleteuser", (req, res) => {
-  res.send("deleted user");
+//prefer try catch instead of this
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Unexpected Error");
+  }
 });
 app.listen(4001, () => {
   console.log("App is listening on 4001..");
